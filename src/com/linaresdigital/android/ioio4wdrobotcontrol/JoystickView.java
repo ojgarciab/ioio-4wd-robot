c18c9696199ac7303d857b542cd5a67fd76aa772
Oscar Garcia <oscar.github@linaresdigital.com>
2016-04-13 22:12:42 +0200

    Joystick skeleton View

---
+++ src/com/linaresdigital/android/ioio4wdrobotcontrol/JoystickView.java
@@ -0,0 +1,12 @@
+package com.linaresdigital.android.ioio4wdrobotcontrol;
+
+import android.content.Context;
+import android.view.View;
+
+public class JoystickView extends View {
+
+	public JoystickView(Context context) {
+		super(context);
+	}
+
+}
