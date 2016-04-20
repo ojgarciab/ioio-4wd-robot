d29fcc78c8d5112ae2075d0067e77c706ab20a02
Oscar Garcia <oscar.github@linaresdigital.com>
2016-04-20 23:33:52 +0200

    Enhancement #2: Added new class to implement IOIO setup and loop

---
+++ src/com/linaresdigital/android/ioio4wdrobotcontrol/IoioLoop.java
@@ -0,0 +1,11 @@
+package com.linaresdigital.android.ioio4wdrobotcontrol;
+
+import ioio.lib.util.BaseIOIOLooper;
+
+public class IoioLoop extends BaseIOIOLooper {
+
+	public IoioLoop() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
