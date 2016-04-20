46405c776b2710aee6b13ae690abce0c18880fda
Oscar Garcia <oscar.github@linaresdigital.com>
2016-04-20 23:52:00 +0200

    Enhancement #1: Ended coding joystick functions

--- src/com/linaresdigital/android/ioio4wdrobotcontrol/MainActivity.java
+++ src/com/linaresdigital/android/ioio4wdrobotcontrol/MainActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+	public JoystickView[] joySticks;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		joySticks[0] = (JoystickView) findViewById(R.id.joystickViewT);
+		joySticks[1] = (JoystickView) findViewById(R.id.joystickViewL);
+		joySticks[2] = (JoystickView) findViewById(R.id.joystickViewR);
 	}
 }
