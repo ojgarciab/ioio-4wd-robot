eeb3dcd28d6c197ffcbd67dda59f0080151f0320
Oscar Garcia <oscar.github@linaresdigital.com>
2016-04-20 23:41:29 +0200

    Enhancement #2: Implemented setup and disconnect methods

--- src/com/linaresdigital/android/ioio4wdrobotcontrol/IoioLoop.java
+++ src/com/linaresdigital/android/ioio4wdrobotcontrol/IoioLoop.java
@@ -1,11 +1,31 @@
 package com.linaresdigital.android.ioio4wdrobotcontrol;
 
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.PwmOutput;
+import ioio.lib.api.exception.ConnectionLostException;
 import ioio.lib.util.BaseIOIOLooper;
 
 public class IoioLoop extends BaseIOIOLooper {
+	private PwmOutput[] pwmMotors;
+	private DigitalOutput activityLed;
 
-	public IoioLoop() {
-		// TODO Auto-generated constructor stub
+	@Override
+	protected void setup() throws ConnectionLostException {
+		pwmMotors = new PwmOutput[4];
+		activityLed = ioio_.openDigitalOutput(0, true);
+		pwmMotors[0] = ioio_.openPwmOutput(10, 500);
+		pwmMotors[1] = ioio_.openPwmOutput(11, 500);
+		pwmMotors[2] = ioio_.openPwmOutput(13, 500);
+		pwmMotors[3] = ioio_.openPwmOutput(12, 500);
+	}
+
+	@Override
+	public void disconnected() {
+		pwmMotors[0].close();
+		pwmMotors[1].close();
+		pwmMotors[2].close();
+		pwmMotors[3].close();
+		activityLed.close();
 	}
 
 }
